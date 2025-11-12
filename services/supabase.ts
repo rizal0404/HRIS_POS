fa303efeea67976dacdf81c2258c978192ced19b
rizal0404 <m.rizal0404@gmail.com>
2025-11-12 21:05:23 +0800

    Fix: Memperbaiki env variables Supabase untuk Vite

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Provided Supabase project details
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 // Initialize and export the Supabase client
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase URL and Anon Key must be provided in environment variables.");
